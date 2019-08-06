018d97c989b0c5be23a222873048f0fa2abdf55f
shaynadavis <shaynadavis@pursuit.org>
2019-08-05 22:04:07 -0400

    unit one assessment

--- unitOneAssessment.js
+++ unitOneAssessment.js
@@ -2,48 +2,128 @@ let assert = require('assert')
 
 // Question One:
 
-// Write a function called isOdd that returns whether or not a number is odd.
-// If something that is not a number is passed in, return false.
+// // Write a function called isOdd that returns whether or not a number is odd.
+// // If something that is not a number is passed in, return false.
+//  function isOdd(num) {
+//    if (num % 2 !== 0) {
+//      return true
+//    } else {
+//      return false
+//    }
+//  }
+//
+//
+// // Uncomment out the next line to test your solution
+// console.log(isOdd(5))
+// console.log(isOdd(4))
 
 
-// Uncomment out the next line to test your solution
-// runQ1Tests()
+
+
 
 
 // Question Two:
 
 // Write a function called numberOfDigits that returns how many digits are in a given number
+//create a string of the number by pushing the numbers into an empty string
+//add all the string elements
 
-// Uncomment out the next line to test your solution
-// runQ2Tests()
+// let num = 329385
+//
+// const numberOfDigits = num => num.toString().length //num.length
+// console.log(typeof numberOfDigits(num))
+//
+// // Uncomment out the next line to test your solution
+// console.log(numberOfDigits(num))
 
-// Question Three:
+
+
+
+
+
+// Question Three:Couldnt figure out the syntax
 
 // Write a function called disemvowel that removes all of the vowels from a string.
 // Treat y as a consonant, not a vowel
 
+//seperate each character into its own string
+//figure out which letters are vowels
+//push them from the string
+//return string
+
+let str = "How are you doing today"
+
+const disemvowel = str => str.forEach(word => !word.includes("a","e","i","o","u"))
+
+
 // Uncomment out the next line to test your solution
-// runQ3Tests()
+console.log(disemvowel(str))
+
+
+
+
+
 
 // Question Four:
 // Write a function called secondSmallest that returns the second smallest number in an array
 
+//iterate through the array
+//find the smallest number
+//compare the second smallest number to the smallest
+//log the second smallest number
+
+// let arr = [3,8,5,9,2]
+// const secondSmallest = (values) => {
+//     let smallest = Infinity;
+//     for (let i = 0; i < arr.length; i++) {
+//       if(values[i] < smallest) {
+//         smallest = values[i];
+//       }
+//       return smallest
+//   }
+// }
+
+
 // Uncomment out the next line to test your solution
-// runQ4Tests()
+// console.log(secondSmallest(arr))
+
+
+
+
+
 
 // Question Five:
 // Write a function called getLocations that takes in an array of objects that look like the array below,
 // and returns an array of the strings corresponding to the value of the location property
 // The output should be in the same order as the input
 
+
+//Create an empty array
+//take the values of the location
+//push them into the new array
+
 // Sample input:
-// [{location: "Algeria", population: 41}, {location: "Belize", population: 0.4}, {location: "China", population: 1386}, {location: "Denmark", population: 6}]
+// let locaArr = [
+//   {location: "Algeria", population: 41},
+//   {location: "Belize", population: 0.4},
+//   {location: "China", population: 1386},
+//   {location: "Denmark", population: 6}
+// ]
+//
+//
+// let locations = []
+//
+// locaArr.forEach(locaArr => {
+//   locations.push(locaArr.location)
+// })
+//
+//
+// // Uncomment out the next line to test your solution
+// console.log(locations)
 
-// Sample output:
-// ["Algeria", "Belize", "China", "Denmark"]
 
-// Uncomment out the next line to test your solution
-// runQ5Tests()
+
+
 
 
 // Question Six:
@@ -51,8 +131,16 @@ let assert = require('assert')
 // Write a function called onlyOddStrings that takes in an array of strings as input and returns an array that only includes strings with an odd number of characters
 // Your function should use a higher-ordered function (e.g map, filter, reduce, every, sort) in its implementation
 
-// Uncomment out the next line to test your solution
-// runQ6Tests()
+// create an array
+// .filter method to loop through the array
+// use a module to figure out if length of word is isOdd
+// log the function calling the array
+
+
+// let arr = ["hello", "there", "mister", "and", "miss"]
+// const onlyOddStrings = arr => arr.filter(word => word.length % 2 !== 0)
+// // Uncomment out the next line to test your solution
+// console.log(onlyOddStrings(arr))
 
 
 // Question Seven:
@@ -62,17 +150,43 @@ let assert = require('assert')
 // Give it two properties set by the constructor named temperature and weather
 // Give it a method named getDescription that returns a string in the format described below
 
+// class Day {
+//   constructor(temperature, weather) {
+//     this.temperature = temperature;
+//     this.weather = weather;
+//   }
+//   getDescription() {
+//     console.log(`It is ${this.temperature} degrees and ${this.weather}`)
+//   }
+// }
+// let today = new Day(65, "cloudy")
+// let thisday = new Day(90, "humid")
+
+// console.log(thisday)
+// console.log(today)
+// console.log(today.getDescription())
+// console.log(thisday.getDescription())
+
 // Example
 // let myDay = Day(80, "sunny")
 // myDay.getDescription() // returns "It is 80 degrees and sunny"
 
-//b.
+//b. COULDNT FIGURE OUT 
 // Make a function called getAllDayDescriptions that takes in an array of Day objects and returns an array of their descriptions.  Use a higher-ordered function (e.g map, filter, reduce, every, sort) in your implementation.
 // The output should be in the same order as the input
 
+
+// let days = [
+//   let today = new Day(75, "sunny"),
+//   let thisday = new Day(90, "humid"),
+//   let yesterday = new Day(55, "raining")
+// ]
+
+// console.log(days)
 // Uncomment out the next line to test your solution
 // runQ7Tests()
 
+// const getAllDayDescriptions = obj => obj.forEach()
 
 
 // The code below is used to test your solutions.  Feel free to look over it, but do not change any of it.
